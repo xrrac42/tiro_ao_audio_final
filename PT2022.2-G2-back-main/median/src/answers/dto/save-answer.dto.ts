a2a11ef860aa9368159fad861ed62e043af269e5
Carlos Eduardo Xavier <65977793+xrrac42@users.noreply.github.com>
2023-11-22 10:48:43 -0300

    Delete PT2022.2-G2-back-main directory

--- PT2022.2-G2-back-main/median/src/answers/dto/save-answer.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType, PickType } from '@nestjs/mapped-types';
-import { AnswerEntity } from '../entities/answer.entity';
-
-export class SaveAnswerDto extends PartialType(
-  PickType(AnswerEntity, ['content']),
-) {}
